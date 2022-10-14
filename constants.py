1d921837e599981a2827b6501b7a9b0da930346c
ignaciopineyro <ignaciopineyroo@gmail.com>
2022-10-14 20:30:20 -0300

    checkpoint

--- constants.py
+++ constants.py
@@ -1,12 +1,19 @@
+import os
 from enum import Enum
 
 
-class MemristorModels(Enum):
-    PERSHIN = 'PERSHIN'
-    PERSHIN_VOURKAS = 'PERSHIN_VOURKAS'
-    BIOLEK = 'BIOLEK'
+PATH = os.path.dirname(__file__)
+MODELS_DIR = f'{PATH}/models/'
+SIMULATIONS_DIR = f'{PATH}/simulation_results/'
 
-class ModelsPath(Enum):
-    PERSHIN =
-    PERSHIN_VOURKAS =
-    BIOLEK
+
+class MemristorModels(Enum):
+    PERSHIN = 'pershin.sub'
+    PERSHIN_VOURKAS = 'pershin_vourkas.sub'
+    BIOLEK = 'biolek.sub'
+
+
+class SimulationFileNames(Enum):
+    PERSHIN = 'pershin_simulation.cir'
+    PERSHIN_VOURKAS = 'pershin_vourkas_simulation.cir'
+    BIOLEK = 'biolek_simulation.cir'
--- services/spicesimulationservice.py
+++ services/spicesimulationservice.py
@@ -1,14 +1,40 @@
-from constants import MemristorModels
+from constants import MemristorModels, MODELS_DIR, SIMULATIONS_DIR
 
 
-class SingleDeviceSimulation:
-    def __init__(self, model: MemristorModels, parameters: dict, directory: str):
+class NetlistSimulation:
+    def __init__(self, model: MemristorModels, parameters: dict, export_directory: str):
         self.model = model
+        self.model_dir = f'{MODELS_DIR}/{model}'
         self.parameters = parameters
-        self.directory = directory
+        self.export_directory = export_directory
+
+    @staticmethod
+    def _get_simulation_file_name(model: MemristorModels) -> str:
+        if model == MemristorModels.PERSHIN:
+            return 'pershin_simulations/pershin_simulation.cir'
+        elif model == MemristorModels.PERSHIN_VOURKAS:
+            return 'pershin_vourkas_simulations/pershin_vourkas_simulation.cir'
+        elif model == MemristorModels.BIOLEK:
+            return 'biolek_simulations/biolek_simulation.cir'
+        else:
+            raise InvalidMemristorModel(f'The model {model} is not valid')
 
     def create_netlist(self):
+        simulation_file_path = f'{SIMULATIONS_DIR}/{self._get_simulation_file_name(self.model)}'
+        with open(simulation_file_path, "w+") as f:
+            f.write(f'MEMRISTOR SIMULATION - MODEL {self.model}\n')
+            f.write(f".include {self.model_dir}\n")
+
+    def write_single_device_simulation(self):
         raise NotImplemented()
 
     def export_data(self):
         raise NotImplemented()
+
+
+class InvalidMemristorModel(Exception):
+    pass
+
+
+netlist_simulation = NetlistSimulation(MemristorModels.PERSHIN, {}, '')
+netlist_simulation.create_netlist()

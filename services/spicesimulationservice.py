800d18bdef751f191eee699c29b63c7059b10ab2
ignaciopineyro <ignaciopineyroo@gmail.com>
2022-10-21 20:19:40 -0300

    checkpoint

--- services/spicesimulationservice.py
+++ services/spicesimulationservice.py
@@ -1,3 +1,5 @@
+from typing import TextIO
+
 from constants import MemristorModels, SIMULATIONS_DIR, WaveForms, AnalysisType, ModelsSimulationFolders
 from representations import InputParameters, ModelParameters, SimulationParameters, DeviceParameters, ExportParameters
 
@@ -25,26 +27,50 @@ class NetlistSimulation:
         else:
             raise InvalidMemristorModel(f'The model {model} is not valid')
 
-    def create_netlist(self):
+    def _write_dependencies(self, f: TextIO):
+        f.write("\n\n* DEPENDENCIES:\n")
+        f.write(f".include {self.model_dir}")
+
+    def _write_components(self, file: TextIO):
+        file.write("\n\n* COMPONENTS:\n")
+        file.write(self.input_parameters.get_voltage_source(self.input_parameters))
+        file.write(self.device_parameters.get_device(self.device_parameters))
+
+    def _write_analysis_commands(self, file: TextIO):
+        file.write("\n\n* ANALYSIS COMMANDS:\n")
+        file.write(self.simulation_parameters.get_analysis(self.simulation_parameters))
+
+    def _write_control_commands(self, file: TextIO):
+        file.write("\n\n* CONTROL COMMANDS:\n")
+        file.write('.control\n')
+        file.write('run\n')
+        file.write('set wr_vecnames\n')
+        file.write('set wr_singlescale\n')
+        file.write(self.export_parameters.get_export_parameters(self.export_parameters))
+        # TODO: AGREGAR TIEMPO DE SIMULACION EN SPICE
+
+    def write_circuit_file(self) -> None:
+        """
+        Writes the .cir circuit file to execute in Spice. The file is saved in simulation_results/model-name_simulations
+        :return: None
+        """
         simulation_file_path = f'{SIMULATIONS_DIR}/{self._get_simulation_file_name(self.model)}'
         with open(simulation_file_path, "w+") as f:
             f.write(f'MEMRISTOR SIMULATION - MODEL {self.model.value}')
-            f.write("\n\n* DEPENDENCIES:\n")
-            f.write(f".include {self.model_dir}")
-            f.write("\n\n* COMPONENTS:\n")
-            f.write(self.input_parameters.get_voltage_source(self.input_parameters))
-            f.write(self.device_parameters.get_device(self.device_parameters))
-            f.write("\n\n* ANALYSIS COMMANDS:\n")
-            f.write(self.simulation_parameters.get_analysis(self.simulation_parameters))
-            f.write("\n\n* CONTROL COMMANDS:\n")
-            f.write('.control\n')
-            f.write('run\n')
-            f.write('set wr_vecnames\n')
-            f.write('set wr_singlescale\n')
-            f.write(self.export_parameters.get_export_parameters(self.export_parameters))
-            # TODO: AGREGAR TIEMPO DE SIMULACION
+            self._write_dependencies(f)
+            self._write_components(f)
+            self._write_analysis_commands(f)
+            self._write_control_commands(f)
             f.write('\n.endc\n')
             f.write('.end\n')
+
+    def write_model_subcircuit(self):
+        """
+        Writes the .sub subcircuit file to include on circuit's file. The file is saved in models/
+        :return: None
+        """
+
+        raise NotImplemented()
 
     def write_single_device_simulation(self):
         raise NotImplemented()
@@ -68,4 +94,4 @@ netlist_simulation = NetlistSimulation(
     MemristorModels.PERSHIN, input_params, model_params, device_params, simulation_params, export_params
 )
 
-netlist_simulation.create_netlist()
+netlist_simulation.write_circuit_file()

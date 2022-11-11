18f6ff0a4604329e6bc9fa1c67229c015646a1c9
ignaciopineyro <ignaciopineyroo@gmail.com>
2022-11-11 19:38:34 -0300

    Declarado servicio de Spice, checkpoint

---
+++ services/ngspiceservice.py
@@ -0,0 +1,11 @@
+
+
+class NGSpiceService:
+    def meassure_linux_execution_time(self):
+        pass
+
+    def meassure_python_execution_time(self):
+        pass
+
+    def run_simulation(self):
+        pass
